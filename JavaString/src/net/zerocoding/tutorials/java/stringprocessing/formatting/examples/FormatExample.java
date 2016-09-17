eafffd33c5a0ab92104b66396ba30cfaea3998e7
youngsuPark <nice2seeu86@gmail.com>
2016-09-17 17:40:41 +0900

    String Format 사용해보기

---
+++ JavaString/src/net/zerocoding/tutorials/java/stringprocessing/formatting/examples/FormatExample.java
@@ -0,0 +1,51 @@
+package net.zerocoding.tutorials.java.stringprocessing.formatting.examples;
+
+public class FormatExample {
+	public static void main(String[] args) {
+		/*
+		 * "%"기호란 : PlaceHolder
+		 *  사용 방법 : % (수식) (자릿수) 타입 
+		 */
+		
+		/*
+		 * 기본 사용
+		 */
+//		{
+//			String tmpStr1 = String.format("정수 표현 : %d", 50);
+//			String tmpStr2 = String.format("문자열 표현 : %s", "안녕하세요");
+//			String tmpStr3 = String.format("소수점 표현 : %f", 1.3333);
+//			String tmpStr4 = String.format("진위 값 표현 : %b", true);
+//			System.out.println(tmpStr1);
+//			System.out.println(tmpStr2);
+//			System.out.println(tmpStr3);
+//			System.out.println(tmpStr4);			
+//		}
+		/*
+		 * 응용 사용1
+		 */
+//		{
+//			final String FORMAT = "%10s";
+//			String getValue1 = "Ayako";
+//			String getValue2 = "YoungSu";
+//			String tmpStr = String.format(FORMAT, getValue1);
+//			String tmpStr2 = String.format(FORMAT, getValue2);
+//			System.out.println(tmpStr);
+//			System.out.println(tmpStr2);
+//		}
+		/*
+		 * 응용 사용2
+		 */
+//		{
+//			final String FORMAT1 = "%,d";
+//			final String FORMAT2 = "%10d";
+//			final String FORMAT3 = "%3.1f";
+//			final String FORMAT4 = "%02d"; // 정수 두자리 숫자 (한자리가 오면 앞에는 0을 채움)
+//			float getValue = 100;
+//			double getValue2 = 99.15;
+//			String tmpStr = String.format(FORMAT3, getValue);
+//			String tmpStr2 = String.format(FORMAT3, getValue2);
+//			System.out.printf("%5s\n",tmpStr);
+//			System.out.printf("%5s",tmpStr2);	
+//		}		
+	}
+}
